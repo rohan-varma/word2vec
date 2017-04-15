5734fb82f3262feea5bc3278c187de8d169969c3
Rohan Varma <rvarm1@gmail.com>
2017-04-14 21:26:32 -0700

    NLP on IMDB dataset

--- vocab.py
+++ vocab.py
@@ -1,17 +1,34 @@
 from collections import defaultdict, Counter
 import numpy as np
-from queue import Queue, PriorityQueue
+from nltk.corpus import stopwords
 
 def make_dict_from_data(data_file):
 	with open(data_file, 'r') as f:
 		data = f.readlines()
 	vocab = defaultdict(int)
 	for line in data:
-		words = line.strip().split(" ") #strip removes trailing and leading whitespace, tabs, newlines, etc
+		words = line.strip().split() #strip removes trailing and leading whitespace, tabs, newlines, etc
 		# the " " param is optional here
 		for word in words:
 			vocab[word]+=1
 	return vocab
+
+def encode_word(word, vocab):
+	vocab_words = list(vocab.keys())
+	assert(word in vocab_words)
+	a = np.zeros(shape = len(vocab_words))
+	idx = vocab_words.index(word)
+	a[idx] = 1
+	return a
+
+def encode_all_words(vocab):
+	vocab_words = list(vocab.keys())
+	encodings = []
+	for word in vocab_words:
+		encodings.append(encode_word(word, vocab))
+	return encodings
+
+
 
 
 
@@ -32,6 +49,16 @@ if __name__ == '__main__':
 	data_file = 'data/input.txt'
 	vocab = make_dict_from_data(data_file)
 	vocab = Counter(vocab)
-	common = vocab.most_common()
-	print(len(common))
-	print(len(vocab))
+	print(len(vocab.keys()))
+	common = vocab.most_common(100)
+	stop = stopwords.words("english")
+	common, stop = set(common), set(stop)
+	for k in list(vocab.keys()):
+		if k in stop or k in common:
+			try:
+				del vocab[k]
+			except KeyError:
+				pass
+	encoded_words = encode_all_words(vocab)
+	print(encoded_words.shape)
+
