cb6d085c90657fd6551107455e90665ee062f401
Rohan Varma <rvarm1@gmail.com>
2017-06-18 16:36:09 -0700

    some progress

--- rnn/rnn_model.py
+++ rnn/rnn_model.py
@@ -5,7 +5,24 @@ import pickle
 def generate_review_features(review, word_to_idx_dict, embeddings):
     x = []
     for word in review.split():
-        embedding = embeddings[word_to_idx_dict[word]]
+        try:
+            embedding = embeddings[word_to_idx_dict[word]]
+        except KeyError:
+            print("KeyError: {} not in embeddings, using last embedding as default".format(word))
+            last_word = list(word_to_idx_dict.keys())[-1]
+            embedding = embeddings[word_to_idx_dict[last_word]]
+        x.append(embedding)
+    return np.array(x)
+
+def generate_batch(features_train_clean_review, batch_size = 64):
+    batch = []
+    for i in range(batch_size):
+        idx = np.random.randint(0, features_train_clean_review.shape[0])
+        batch.append(features_train_clean_review[idx])
+    return np.array(batch)
+
+
+
 if __name__ == '__main__':
     # the following code just reads in the word embeddings and a mapping from word to vector.
     try:
@@ -24,7 +41,10 @@ if __name__ == '__main__':
         print("please run word2vec.py which will load and save the data")
 
     # variables needed later
-    num_clusts = 500
     word_to_idx_dict = {v: k for k, v in reverse_dictionary.items()}
-    word = list(word_to_idx_dict.keys())[15]
-    print("Word {} has index {} which coressponds to embedding: {}".format(word, word_to_idx_dict[word], final_embeddings[word_to_idx_dict[word]]))
+    features_train_clean_review = [generate_review_features(rev, word_to_idx_dict, final_embeddings) for rev in train_cleaned_reviews]
+    features_train_clean_review = np.array(features_train_clean_review)
+    print(features_train_clean_review[0].shape)
+    batch = generate_batch(features_train_clean_review, batch_size = 10)
+    print(batch.shape[0])
+    print(batch[0].shape)
